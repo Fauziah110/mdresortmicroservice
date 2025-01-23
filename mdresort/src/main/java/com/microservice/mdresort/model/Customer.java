3f99ae411258a2d73f61a01b434f30d34f166805
Fauziah110 <2024159635@student.uitm.edu.my>
2025-01-24 02:33:12 +0800

    Add files via upload

---
+++ mdresort/src/main/java/com/microservice/mdresort/model/Customer.java
@@ -0,0 +1,67 @@
+package com.microservice.mdresort.model;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Customer_microservices")
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Integer customerId;
+
+    @Column(name = "customer_name", nullable = false)
+    private String customerName;
+
+    @Column(name = "customer_email", nullable = false, unique = true)
+    private String customerEmail;
+
+    @Column(name = "customer_phone_no", nullable = false)
+    private String customerPhoneNo;
+
+    @Column(name = "customer_password", nullable = false)
+    private String customerPassword;
+
+    // Getters and Setters
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String firstName) {
+        this.customerName = firstName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhoneNo() {
+        return customerPhoneNo;
+    }
+
+    public void setCustomerPhoneNo(String customerPhoneNo) {
+        this.customerPhoneNo = customerPhoneNo;
+    }
+
+    public String getCustomerPassword() {
+        return customerPassword;
+    }
+
+    public void setCustomerPassword(String customerPassword) {
+        this.customerPassword = customerPassword;
+    }
+}
